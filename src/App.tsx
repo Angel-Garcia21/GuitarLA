32a26cdb0b0ac633ee2f38c924641037f1537e7b
AngelDev <angel.programador21@gmail.com>
2024-05-18 07:10:25 -0600

    Primera version con TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+
+import { Header } from "./components/Header"
+import { Guitar } from "./components/Guitar"
+import { useCart } from "./hooks/useCart"
+
+
+function App() {
+
+  const { 
+    data,
+    cart,
+    addToCart,
+    removeFromCart,
+    increaseQuantity,
+    decreaseQuantity,
+    clearCart,
+    isEmpty,
+    cartTotal
+} = useCart()
+
+
+
+  return (
+    <> 
+    <Header
+    cart={cart}
+    removeFromCart={removeFromCart}
+    increaseQuantity={increaseQuantity}
+    decreaseQuantity={decreaseQuantity}
+    clearCart={clearCart}
+    isEmpty={isEmpty}
+    cartTotal={cartTotal}
+    />
+
+    
+    <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+        {data.map((guitar) => (
+          <Guitar
+          key={guitar.id}
+          guitar={guitar}
+          addToCart={addToCart}
+          />
+        
+        ))}
+
+        </div>
+    </main>
+
+
+    <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+            <p className="text-white  fs-4 mt-4 m-md-0 ">Angel Garcia 2024 - Derechos Reservados</p>
+        </div>
+
+    </footer>
+    </>
+  )
+}
+
+export default App
